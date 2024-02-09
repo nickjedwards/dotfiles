9fc41dd8224f9e1fa2c239f87353844b2d7462cf
Nick Edwards <46484451+nickjedwards@users.noreply.github.com>
2024-02-09 17:35:04 +1100

    feat: let's get rusty

--- .config/nvim/lua/custom/configs/lspconfig.lua
+++ .config/nvim/lua/custom/configs/lspconfig.lua
@@ -1,10 +1,18 @@
 local config = require "plugins.configs.lspconfig"
 
 local lspconfig = require "lspconfig"
+local util = require "lspconfig/util"
 
 lspconfig.rust_analyzer.setup({
   on_attach = config.on_attach,
   capabilities = config.capabilities,
   filetypes = {"rust"},
-  root_dir = lspconfig.util.root_pattern("Cargo.toml"),
+  root_dir = util.root_pattern("Cargo.toml"),
+  settings = {
+    ['rust-analyzer'] = {
+      cargo = {
+        allFeatures = true,
+      },
+    },
+  },
 })
--- .config/nvim/lua/custom/plugins.lua
+++ .config/nvim/lua/custom/plugins.lua
@@ -18,6 +18,13 @@ local plugins = {
       require "custom.configs.lspconfig"
     end,
   },
+  {
+    "rust-lang/rust.vim",
+    ft = "rust",
+    init = function ()
+      vim.g.rustfmt_autosave = 1
+    end
+  },
 }
 
 return plugins
