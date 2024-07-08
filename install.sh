85524a674850e3404bd233775b9b073b55e7a3ef
Nick Edwards <46484451+nickjedwards@users.noreply.github.com>
2024-07-08 18:42:06 +1000

    hypr

--- install.sh
+++ install.sh
@@ -28,7 +28,7 @@ yes '' | sudo pacman -S bat \
     zsh
 
 # Install AUR packages
-yes '' | paru -S flameshot-git nerdfetch rofi-wayland swaylock-effects waybar wlogout
+yes '' | paru -S bibata-cursor-theme-bin flameshot-git nerdfetch rofi-wayland swaylock-effects waybar wlogout
 
 # Zsh && Oh My ZSH!
 chsh -s $(which zsh)
