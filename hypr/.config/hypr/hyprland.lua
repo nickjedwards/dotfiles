e5b6936ded3a78dc49d3c2dbbe6e0b7842d0434c
Nick Edwards <46484451+nickjedwards@users.noreply.github.com>
2026-05-16 17:15:26 +1000

    fix: 🐛 adds authentication agent

--- hypr/.config/hypr/hyprland.lua
+++ hypr/.config/hypr/hyprland.lua
@@ -36,6 +36,7 @@ hl.on("hyprland.start", function ()
     hl.exec_cmd("gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita-dark'")
     hl.exec_cmd("gsettings set org.gnome.desktop.interface icon-theme 'Numix-Circle'")
 
+    hl.exec_cmd("/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1")
     hl.exec_cmd("wayle panel start")
     hl.exec_cmd("hyprpaper & hypridle")
     hl.exec_cmd("wl-paste --type text --watch cliphist store") -- Stores only text data
